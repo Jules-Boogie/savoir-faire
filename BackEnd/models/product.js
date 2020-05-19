9aa44dcce46651c4a3d145fffa7f32dcdc0e81f6
Juliet George <georgejuliet11@gmail.com>
2020-05-19 12:23:38 -0700

    created models

---
+++ BackEnd/models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productsSchema = new Schema({
+    Name: { type: String, required: true },
+    Description: { type: String},
+    Price: { type: Number, required:true},
+    Brand:{type: String, required:true}
+  });
+  
+  const Product  = mongoose.model("Product", productsSchema);
+  
+  module.exports = Product;
+  
--- BackEnd/models/users.js
+++ BackEnd/models/users.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required:true},
+    products:[[{ type: Schema.Types.ObjectId, ref: 'products' }]]
+  });
+  
+  const User  = mongoose.model("User", userSchema);
+  
+  module.exports = User;
+  
