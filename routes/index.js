a3025d8deb77bb5d6a93281de1830078c8533794
Juliet George <georgejuliet11@gmail.com>
2020-06-03 15:24:19 -0700

    removing node modules

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const apiRoutes = require("./APIRoutes");
 // API Routes
 router.use("/api", apiRoutes);
 
-// router.use(function(req, res) {
-//     res.sendFile(path.join(__dirname, "../../savoire-faire/public/index.html"));
-//   });
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/public/index.html"));
+  });
   
   
 module.exports = router;
