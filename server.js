b2b9e11ff680eef4288e4ce282484d7217ec6b1a
Juliet George <georgejuliet11@gmail.com>
2020-06-03 22:44:27 -0700

    change directory

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 3002;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/public"));
+// }
 
 
 if (process.env.NODE_ENV === "production") {
