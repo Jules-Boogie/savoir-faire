b8c685dfbf9281e2c4f730491ba42223be8fb8d6
Juliet George <georgejuliet11@gmail.com>
2020-06-04 03:24:32 -0700

    change directory

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const apiRoutes = require("./APIRoutes");
 router.use("/api", apiRoutes);
 
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
   
   
